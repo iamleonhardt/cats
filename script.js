b51a33fcaded72bbbd3e8669cea6bb2d2a81740f
Bill Leonhardt <iamleonhardt@gmail.com>
2017-01-05 01:07:34 -0800

    cat walks all directions with sprite sheet

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 /**
  * Created by Bill on 1/4/17.
  */
+
+
+$(document).keydown('keypress', function (e) {
+    console.log(e.which);
+
+    switch (e.which) {
+        case 38: //up
+            $('#cat').addClass('up');
+            break;
+        case 37: //left
+            $('#cat').addClass('left');
+            break;
+        case 40: // down
+            $('#cat').addClass('down');
+            break;
+        case 39: // right
+            $('#cat').addClass('right');
+            break;
+    }
+});
+
+$(document).keyup(function(){
+    $('#cat').removeClass();
+    $('#cat').addClass('stand');
+});
