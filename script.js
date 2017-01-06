f87e67819521d04b1c0572f467a9e3c65a66f099
Bill Leonhardt <iamleonhardt@gmail.com>
2017-01-05 20:54:46 -0800

    sprite moves, added different sprites

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 /**
  * Created by Bill on 1/4/17.
  */
+var catLeft = 0;
+var catTop = 0;
+var speed = 8;
 
 
-$(document).keydown('keypress', function (e) {
+
+$(document).keydown(move);
+
+function move(e) {
     console.log(e.which);
 
     switch (e.which) {
-        case 38: //up
-            $('#cat').addClass('up');
-            break;
         case 37: //left
             $('#cat').addClass('left');
+            catLeft -= 1;
+            console.log('cat is : ', cat);
+            console.log('cat.style is : ', cat.style);
+            cat.style.left = (catLeft * speed) + 'px';
             break;
-        case 40: // down
-            $('#cat').addClass('down');
+
+        case 38: //up
+            $('#cat').addClass('up');
+            catTop -= 1;
+            cat.style.top = (catTop * speed) + 'px';
             break;
+
         case 39: // right
             $('#cat').addClass('right');
+            catLeft++;
+            cat.style.left = (catLeft * speed) + 'px';
+            break;
+
+        case 40: // down
+            $('#cat').addClass('down');
+            catTop++;
+            cat.style.top = (catTop * speed) + 'px';
             break;
     }
-});
+};
 
 $(document).keyup(function(){
     $('#cat').removeClass();
