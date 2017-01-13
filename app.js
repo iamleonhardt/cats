af33235db32f30f6ed8ce806cc8d7e0985d673cb
Bill Leonhardt <iamleonhardt@gmail.com>
2017-01-12 23:30:44 -0800

    http writing html

--- app.js
+++ app.js
@@ -3,22 +3,28 @@
  */
 
 
-console.log('Mushies Great Adventures');
+// console.log('Mushies Great Adventures');
 
 
-var express = require('express');
-var app = express();
-var serv = require('http').Server(app);
+var http = require('http');
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<!DOCTYPE html>\n<html lang="en">\n' +
+        '<head>\n' +
+        '<meta charset="UTF-8">\n' +
+        '<link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">\n' +
+        '<link rel="stylesheet" href="style.css">\n' +
+        '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>\n' +
+        '<script src="js/script.js"></script>\n' +
+        '<script src="js/gameController.js"></script>\n' +
+        '<script src="js/tileMap.js"></script>\n' +
+        '<script src="js/heroController.js"></script>\n' +
+        '<title>Title</title>\n' +
+        '</head>\n' +
+        '<body>\n' +
+        '<div id="gameArea"></div>\n' +
+        '</body>\n' +
+        '</html>');
+    res.end();
 
-app.get('/',function(req, res){
-    res.sendFile(__dirname + '/client/index.html');
-});
-app.use('/client',express.static(__dirname + '/client'));
-
-serv.listen(2000);
-
-
-var io = require('socket.io')(serv,{});
-io.sockets.on('connection', function(socket){
-    console.log('socket connection');
-})
+}).listen(2000, '127.0.0.1');
