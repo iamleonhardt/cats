7e65dcfeb0177b2335aee6354fb83fa40ed1b0c9
Bill Leonhardt <iamleonhardt@gmail.com>
2017-01-28 21:33:08 -0800

    node merging

--- app.js
+++ app.js
@@ -9,22 +9,23 @@ console.log('Mushies Great Adventures');
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+var io = require('socket.io')(serv,{});
+serv.listen(2000);
+
 
 app.get('/',function(req, res){
     res.sendFile(__dirname + '/client/index.html');
 });
-app.use('/client',express.static(__dirname + '/client'));
-
-serv.listen(2000);
+// app.use('/client',express.static(__dirname + '/client'));
 
 
-var io = require('socket.io')(serv,{});
+
 
 io.sockets.on('connection', function(socket){
     console.log('socket connection');
 
     socket.on('happy', function(data){
-        console.log('I am happy because ' +data.reason);
+        console.log('I am happy because ' + data.reason);
     })
 
     socket.emit('serverMsg', {
